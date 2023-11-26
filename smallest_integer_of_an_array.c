f081e91a66b26e03e20f49e4ad6af1b813d7a602
SelimYuksel <77573218+SelimYuksel@users.noreply.github.com>
2023-11-26 10:43:33 +0300

    Add files via upload

---
+++ smallest_integer_of_an_array.c
@@ -0,0 +1,42 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int find_smallest_int(int array[], size_t length) {
+	
+	if(length == 0) {        //If the length of the array is zero, stop the program
+		return 0;
+	}
+	
+	int smallest = array[0];
+	
+	for(size_t i = 0; i < length; i++) {
+		if(array[i] < smallest) {
+			smallest = array[i];
+		}
+	}
+	
+	return smallest;
+}
+
+int main() {
+	
+	int size;
+	
+	printf("Enter the size: ");
+	scanf("%d", &size);
+	
+	int numbers[size]; //Declare 'numbers' after getting 'size'
+	
+	for (int i = 0; i < size; i++) {
+		printf("Enter the number %d: ", i+1);
+		scanf("%d", &numbers[i]);
+	}
+	
+	int smallest = find_smallest_int(numbers, size);
+	
+	printf("Smallest number is: %d\n", smallest);
+	
+	return 0;
+		
+}
